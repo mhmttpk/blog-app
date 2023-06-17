4004e1f7ec8a461ed35c4f3af45f4814a26c0656
mhmttpk <mttopak@gmail.com>
2023-06-17 22:39:51 +0300

    Footer has been configured

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './footer.module.css';
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
         <div>&#169; All rights reserved.</div>
+        <div className={styles.social}>
+            <Image src="/facebook.png" width={15} height={15} className={styles.icon} alt="Facebook"/>
+            <Image src="/instagram.png" width={15} height={15} className={styles.icon} alt="Instagram"/>
+            <Image src="/twitter.png" width={15} height={15} className={styles.icon} alt="Twitter"/>
+            <Image src="/youtube.png" width={15} height={15} className={styles.icon} alt="Youtube"/>
+        </div>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
