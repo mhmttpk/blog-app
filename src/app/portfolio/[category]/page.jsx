87c2b12a9450e4dfd53f27734372ef5784fb8e5b
mhmttpk <mttopak@gmail.com>
2023-06-22 00:58:40 +0300

    Portfolio page has been configured

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css';
 
-const Category = () => {
-  return (
-    <div>Category</div>
-  )
+const Category = ({params}) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.catTitle}>{params.category}</h1>
+            <div className={styles.item}>
+            <div className={styles.content}></div>
+            <div className={styles.imgContainer}></div>
+            </div>
+        </div>
+    )
 }
 
-export default Category
+export default Category;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,8 +7,8 @@ const Portfolio = () => {
         <div className={styles.container}>
             <h1 className={styles.selectTitle}>Choose a gallery</h1>
             <div className={styles.items}>
-                <Link href="/portfolio/data-analysis" className={styles.item}>
-                    <span className={styles.title}>Data Analysis</span>
+                <Link href="/portfolio/databases" className={styles.item}>
+                    <span className={styles.title}>Databases</span>
                 </Link>
                 <Link href="/portfolio/websites" className={styles.item}>
                     <span className={styles.title}>Websites</span>
