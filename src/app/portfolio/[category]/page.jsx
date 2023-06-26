f1e1b856b3fe36bf9491bfb41ce2ebb8916b6518
mhmttpk <mttopak@gmail.com>
2023-06-26 23:58:13 +0300

    category page structure

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './page.module.css';
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
 const Category = ({params}) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.category}</h1>
             <div className={styles.item}>
-            <div className={styles.content}></div>
-            <div className={styles.imgContainer}></div>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Test</h1>
+                <p className={styles.desc}>Desc</p>
+                <Button text="See More" url="#"/>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} fill={true} src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg" alt=""/>
+            </div>
             </div>
         </div>
     )
