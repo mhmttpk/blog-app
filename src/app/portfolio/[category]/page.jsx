d68505ac834b9e878eff16d1027fe8461ac477c7
mhmttpk <mttopak@gmail.com>
2023-06-28 23:39:52 +0300

    Category Pages has been configured

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -8,14 +8,34 @@ const Category = ({params}) => {
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.category}</h1>
             <div className={styles.item}>
-            <div className={styles.content}>
-                <h1 className={styles.title}>Test</h1>
-                <p className={styles.desc}>Desc</p>
-                <Button text="See More" url="#"/>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See More" url="#"/>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image className={styles.img} fill={true} src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg" alt=""/>
+                </div>
             </div>
-            <div className={styles.imgContainer}>
-                <Image className={styles.img} fill={true} src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg" alt=""/>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See More" url="#"/>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image className={styles.img} fill={true} src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg" alt=""/>
+                </div>
             </div>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See More" url="#"/>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image className={styles.img} fill={true} src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg" alt=""/>
+                </div>
             </div>
         </div>
     )
