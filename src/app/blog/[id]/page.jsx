77916b104e6aca1a8f266e72c795108b08da2978
mhmttpk <mttopak@gmail.com>
2023-06-30 02:02:46 +0300

    Blog page and blogpost detail page has been configured
    I used jsonplaceholder to send request and fetch data to test my pages

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { cache: 'no-store' });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+};
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>
+            {data.body}
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Test</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919.jpeg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         Test Content
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
