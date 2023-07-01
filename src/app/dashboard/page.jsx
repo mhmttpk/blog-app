91367498e448ce8e94b997710585f321d1e15c50
mhmttpk <mttopak@gmail.com>
2023-07-01 23:55:23 +0300

    Data fetching with swr on Dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import styles from './page.module.css';
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
-}
+    
+    const fetcher = (...args) => fetch(...args).then(res => res.json());
 
-export default Dashboard
+    const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+
+    console.log(data);
+
+    return <div className={styles.container}>Dashboard</div>;
+};
+
+export default Dashboard;
